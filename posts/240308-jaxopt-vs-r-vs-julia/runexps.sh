322549051db098946dd657b90d389667d400713a
vanAmsterdam <w.a.c.vanamsterdam@gmail.com>
2024-03-17 12:01:21 +0100

    minor updates

--- posts/240308-jaxopt-vs-r-vs-julia/runexps.sh
+++ posts/240308-jaxopt-vs-r-vs-julia/runexps.sh
@@ -1,23 +1,26 @@
 #!/bin/zsh
 
-for nreps in 10 100
+#for nreps in 1000 100000
+#for nreps in 10000000
+#for nreps in 10000 1000000
+for nreps in 1000 10000 100000 1000000 10000000
 do
     echo $nreps
     #echo "nreps $nreps" >> timings.txt
-    { time Rscript scripts/rspeed.R $nreps ; } 2>> timings.txt
-    sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
     { time julia scripts/jlspeed.jl $nreps ; } 2>> timings.txt
     sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
     { time python scripts/jaxspeed.py $nreps ; } 2>> timings.txt
     sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
+    { time Rscript scripts/rspeed.R $nreps ; } 2>> timings.txt
+    sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
 
-    for nthreads in 2 4
+    for nthreads in 8
     do
         echo $nthreads
         #echo "nthreads $nthreads" >> timings.txt
-        { time Rscript scripts/rspeed.R $nreps $nthreads ; } 2>> timings.txt
-        sed -i '' '$s/$/ '"${nreps}"' nreps '"${nthreads}"' nthreads/' timings.txt
         { time julia -t $nthreads scripts/jlspeed.jl $nreps ; } 2>> timings.txt
+        sed -i '' '$s/$/ '"${nreps}"' nreps '"${nthreads}"' nthreads/' timings.txt
+        { time Rscript scripts/rspeed.R $nreps $nthreads ; } 2>> timings.txt
         sed -i '' '$s/$/ '"${nreps}"' nreps '"${nthreads}"' nthreads/' timings.txt
     done
 done
--- posts/240308-jaxopt-vs-r-vs-julia/scripts/jaxspeed.py
+++ posts/240308-jaxopt-vs-r-vs-julia/scripts/jaxspeed.py
@@ -1,6 +1,6 @@
 import jax, jaxopt
-# jax.config.update('jax_platform_name', 'cpu')
-from jax import numpy as jnp, random, vmap, jit
+jax.config.update('jax_platform_name', 'cpu') # make sure jax doesnt use a gpu if it's available
+from jax import numpy as jnp, random, vmap, jit, lax
 from jaxopt import LBFGS
 from jaxopt.objective import binary_logreg
 from argparse import ArgumentParser
@@ -20,18 +20,24 @@ def make_data(k, n=int(1e3)):
 solver = LBFGS(binary_logreg)
 w_init = jnp.zeros((9,))
 
-@jit # jit toggles just-in-time compilation, one of the main features of JAX
 def solve(k):
     # need to specify parameter initialization values
     data = make_data(k)
     param, state = solver.run(w_init, data)
     return param
 
+# scan is a jax optimized way of looping
+def scannable_solve(carry, k):
+    params = solve(k)
+    return carry, params
+
 if __name__ == '__main__':
     args = parser.parse_args()
     k0 = random.PRNGKey(240316)
     ks = random.split(k0, args.nreps)
-    params = vmap(solve)(ks) # vmap vectorizes computations
+    _, params = lax.scan(scannable_solve, None, ks)
+    # params = vmap(solve)(ks) # vmap vectorizes computations
+    # params = vmap(solve)(ks) # vmap vectorizes computations
     means = jnp.mean(params, axis=0)
     print(means[0])
 

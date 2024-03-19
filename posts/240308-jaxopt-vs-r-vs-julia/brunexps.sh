e83ad9ed43b7aa62a482f2024cc08f67e23b01e6
vanAmsterdam <w.a.c.vanamsterdam@gmail.com>
2024-03-19 17:24:49 +0100

    polishing, almost done

--- posts/240308-jaxopt-vs-r-vs-julia/brunexps.sh
+++ posts/240308-jaxopt-vs-r-vs-julia/brunexps.sh
@@ -3,17 +3,17 @@
 for nreps in 1000 10000 100000 1000000 10000000
 do
     echo $nreps
-    #echo "nreps $nreps" >> bashtimings.txt
     { time python scripts/jaxspeed.py $nreps map; } 2>&1 | grep real >> bashtimings.txt
     sed -i '$s/$/ jax '"${nreps}"' nreps 12 nthreads map primitive/' bashtimings.txt
+    { time python scripts/jaxspeed.py $nreps vmap; } 2>&1 | grep real >> bashtimings.txt
+    sed -i '$s/$/ jax '"${nreps}"' nreps 12 nthreads vmap primitive/' bashtimings.txt
 
     for nthreads in 1 6 12
     do
-	echo $nthreads
-        #echo "nthreads $nthreads" >> timings.txt
-	{ time julia -t $nthreads scripts/jlspeed.jl $nreps ; } 2>&1 | grep real >> bashtimings.txt
-	sed -i '$s/$/ julia '"${nreps}"' nreps '"${nthreads}"' nthreads/' bashtimings.txt
-	{ time Rscript scripts/rspeed.R $nreps $nthreads ; } 2>&1 | grep real >> bashtimings.txt
-	sed -i '$s/$/ r '"${nreps}"' nreps '"${nthreads}"' nthreads/' bashtimings.txt
+	    echo $nthreads
+	    { time julia -t $nthreads scripts/jlspeed.jl $nreps ; } 2>&1 | grep real >> bashtimings.txt
+	    sed -i '$s/$/ julia '"${nreps}"' nreps '"${nthreads}"' nthreads/' bashtimings.txt
+	    { time Rscript scripts/rspeed.R $nreps $nthreads ; } 2>&1 | grep real >> bashtimings.txt
+	    sed -i '$s/$/ r '"${nreps}"' nreps '"${nthreads}"' nthreads/' bashtimings.txt
     done
 done
--- posts/240308-jaxopt-vs-r-vs-julia/runexps.sh
+++ posts/240308-jaxopt-vs-r-vs-julia/runexps.sh
@@ -1,23 +1,16 @@
 #!/bin/zsh
 
-#for nreps in 1000 100000
-#for nreps in 10000000
-#for nreps in 10000 1000000
 for nreps in 1000 10000 100000 1000000 10000000
 do
     echo $nreps
-    #echo "nreps $nreps" >> timings.txt
-    { time julia scripts/jlspeed.jl $nreps ; } 2>> timings.txt
-    sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
-    { time python scripts/jaxspeed.py $nreps ; } 2>> timings.txt
-    sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
-    { time Rscript scripts/rspeed.R $nreps ; } 2>> timings.txt
-    sed -i '' '$s/$/ '"${nreps}"' nreps /' timings.txt
+    { time python scripts/jaxspeed.py $nreps map ; } 2>> timings.txt
+    sed -i '' '$s/$/ '"${nreps}"' nreps 8 threads map primitive/' timings.txt
+    { time python scripts/jaxspeed.py $nreps vmap ; } 2>> timings.txt
+    sed -i '' '$s/$/ '"${nreps}"' nreps 8 threads vmap primitive/' timings.txt
 
-    for nthreads in 8
+    for nthreads in 1 8
     do
         echo $nthreads
-        #echo "nthreads $nthreads" >> timings.txt
         { time julia -t $nthreads scripts/jlspeed.jl $nreps ; } 2>> timings.txt
         sed -i '' '$s/$/ '"${nreps}"' nreps '"${nthreads}"' nthreads/' timings.txt
         { time Rscript scripts/rspeed.R $nreps $nthreads ; } 2>> timings.txt

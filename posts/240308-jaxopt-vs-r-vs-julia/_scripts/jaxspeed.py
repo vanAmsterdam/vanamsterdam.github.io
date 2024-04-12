9ab000d4d83c354ef482c21607406c8f67b5b410
vanAmsterdam <w.a.c.vanamsterdam@gmail.com>
2024-04-12 09:28:44 +0200

    updates to jaxspeed

--- posts/240308-jaxopt-vs-r-vs-julia/_scripts/jaxspeed.py
+++ posts/240308-jaxopt-vs-r-vs-julia/_scripts/jaxspeed.py
@@ -8,6 +8,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument('nreps', nargs="?", type=int, default=int(10))
 parser.add_argument('primitive', nargs="?", type=str, default="vmap")
+parser.add_argument('nscan', nargs="?", type=int, default=int(0))
 
 def make_data(k, n=int(1e3)):
     X_full = random.normal(k, (n,10)) # JAX needs explicit keys for psuedo random number generation
@@ -44,12 +45,21 @@ if __name__ == '__main__':
         params = lax.map(solve, ks)
         means = jnp.mean(params, axis=0)
     elif args.primitive == 'vmap':
-        ks = random.split(k0, args.nreps)
-        params = vmap(solve)(ks)
+        if args.nscan > 0:
+            n_per_vmap = int(args.nreps / args.nscan)
+            def solve_fn(k):
+                carry0 = (k, w_init)
+                (_, params), _ = lax.scan(solve_scan, carry0, None, length=args.nscan)
+                return params / args.nscan
+        else:
+            n_per_vmap = args.nreps
+            solve_fn = solve
+        ks = random.split(k0, n_per_vmap)
+        params = vmap(solve_fn)(ks)
         means = jnp.mean(params, axis=0)
     elif args.primitive == 'scan':
-        carry = (k0, w_init)
-        (k_final, params), _ = lax.scan(solve_scan, carry, None, length=args.nreps)
+        carry0 = (k0, w_init)
+        (_, params), _ = lax.scan(solve_scan, carry0, None, length=args.nreps)
         means = params / args.nreps
     else:
         raise ValueError(f"unrecognized primitive: {args.primitive}, choose map, vmap or scan")
